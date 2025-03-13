c5d470c45167918ddeb76e054074f86759772fbc
angel33la <angel33la@outlook.com>
2025-03-13 13:16:59 -0400

    Update PostCard component with new image, modify text content, and enhance styling for improved aesthetics

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // images
-import Picture from '../images/pixeltrue-jogging-1.svg';
+import Picture from '../images/pixeltrue-yoga.svg';
 
 
 const PostCard = (props) =>{
     return (
       <article style={styles.myArticle}>
         <img src={Picture} alt="Dog Playing"/>
-        <h2>Dogs Are Awesome</h2>
-        <p>Dogs are the best!</p>
+        <h2 style={styles.h2}>Cats Are Awesome</h2>
+        <p style={styles.p}>Cats are the best! I love them!</p>
         <div style={styles.buttons}>
           <span type="submit" style={styles.button}>Like</span>
           <span type="submit" style={styles.button}>Comment</span>
@@ -26,7 +26,7 @@ const styles = {
         alignItems: 'center',
         margin: '20px',
         padding: '20px',
-        border: '2px solid #9b5de5ff',
+        border: '2px solid #00f5d4ff',
         borderRadius: '5px',
         boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
     },
@@ -39,9 +39,15 @@ const styles = {
         padding: '10px',
         borderRadius: '5px',
         backgroundColor: '#00bbf9ff',
-        color: 'white',
+        color: '#000',
         border: '1px solid #fee440ff',
         cursor: 'pointer',
+    },
+    h2: {
+        color: '#f15bb5ff',
+    },
+    p: {
+        color: '#9b5de5ff',
     }
 };
 
