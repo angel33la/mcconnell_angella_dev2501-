28cfe83b933c71fb698c713e7c30aee3ecca7de2
angel33la <angel33la@outlook.com>
2025-03-27 03:40:28 -0400

    Refactor Header and SettingsForm components for improved layout and styling; add theme toggle functionality in Settings page

--- src/components/form/SettingsForm.js
+++ src/components/form/SettingsForm.js
@@ -1,154 +1,146 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../buttons/Button";
-// import { FaPencilAlt } from "react-icons/fa";
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: row;
+  background-color: whitesmoke;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border: 1px solid #ccc;
+  padding: 20px;
+  height: 100vh;
+  width: 99vh;
+`;
+
+const ProfileHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  padding: 1%;
+  background-color: whitesmoke;
+  color: #333;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const InputRow = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  padding: 1%;
+  margin: 1%;
+  margin-right: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const StyledButton = styled(Button)`
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #00bbf9ff;
+  color: #000;
+  border: 1px solid #fee440ff;
+  cursor: pointer;
+`;
 
 const SettingsForm = (props) => {
-    return (
-        <section style={styles.container}>
-            <div style={styles.ProfileHeader}>
-            <img style={styles.avatar} src={props.avatar} alt="avatar" />
-            <h4>{props.username}</h4>
-            <Button style={styles.button} onClick={props.onClick}>
-                Edit Profile
-            </Button>
-        </div>
+  return (
+    <Container>
+      <ProfileHeader>
+        <Avatar src={props.avatar} alt="avatar" />
+        <h4>{props.username}</h4>
+        <StyledButton onClick={props.onClick}>Edit Profile</StyledButton>
+      </ProfileHeader>
 
-        <form style={styles.form} onSubmit={props.editMe}>
-        <p style={styles.myInputRow}>
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="fname"
-                value={props.fname}
-                placeholder="First Name"
-                readOnly
-            />
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="lname"
-                value={props.lname}
-                placeholder="Last Name"
-                readOnly
-            />
-        </p>
-        <p style={styles.myInputRow}>
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="street"
-                value={props.street}
-                placeholder="Street"
-                readOnly
-            />
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="city"
-                value={props.city}
-                placeholder="City"
-                readOnly
-                    />
-            </p>
-            <p style={styles.myInputRow}>
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="state"
-                value={props.state}
-                placeholder="State"
-                readOnly
-            />
-            <Input
-                style={styles.myInput}
-                type="text"
-                name="zip"
-                value={props.zip}
-                placeholder="Zip"
-                readOnly
-            />
-            </p>
-            <p style={styles.myInputRow}>
-            <Input
-                style={styles.myInput}
-                type="email"
-                name="email"
-                value={props.email}
-                placeholder="Email"
-                readOnly
-            />
-            <Input
-                style={styles.myInput}
-                type="phone"
-                name="phone"
-                value={props.phone}
-                placeholder="Phone"
-                readOnly
-            />
-            </p>
-        </form>
-    </section>
-    );
-}
+      <Form onSubmit={props.editMe}>
+        <InputRow>
+          <Input
+            type="text"
+            name="fname"
+            value={props.fname}
+            placeholder="First Name"
+            readOnly
+          />
+          <Input
+            type="text"
+            name="lname"
+            value={props.lname}
+            placeholder="Last Name"
+            readOnly
+          />
+        </InputRow>
+        <InputRow>
+          <Input
+            type="text"
+            name="street"
+            value={props.street}
+            placeholder="Street"
+            readOnly
+          />
+          <Input
+            type="text"
+            name="city"
+            value={props.city}
+            placeholder="City"
+            readOnly
+          />
+        </InputRow>
+        <InputRow>
+          <Input
+            type="text"
+            name="state"
+            value={props.state}
+            placeholder="State"
+            readOnly
+          />
+          <Input
+            type="text"
+            name="zip"
+            value={props.zip}
+            placeholder="Zip"
+            readOnly
+          />
+        </InputRow>
+        <InputRow>
+          <Input
+            type="email"
+            name="email"
+            value={props.email}
+            placeholder="Email"
+            readOnly
+          />
+          <Input
+            type="phone"
+            name="phone"
+            value={props.phone}
+            placeholder="Phone"
+            readOnly
+          />
+        </InputRow>
+      </Form>
+    </Container>
+  );
+};
 
 export default SettingsForm;
-
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: 'whitesmoke',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2)',
-        border: '1px solid #ccc',
-        padding: 20,
-        height: '100vh',
-        width: '99vh',
-    },
-    ProfileHeader: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    avatar: {
-        width: '50px',
-        height: '50px',
-        borderRadius: '50%',
-        objectFit: 'cover'
-    },
-    form: {
-        display: 'flex',
-        flexDirection: 'column',
-        // Added this to expand the width of the form.
-        width: '500px',
-        padding: '2%',
-        backgroundColor: 'whitesmoke',
-        color: '#333',
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    },
-    myInputRow: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: '20px',
-    },
-    myInput: {
-        padding: '2%',
-        margin: '1%',
-        marginRight: '10px',
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-    },
-    button: {
-        padding: '10px',
-        borderRadius: '5px',
-        backgroundColor: '#00bbf9ff',
-        color: '#000',
-        border: '1px solid #fee440ff',
-        cursor: 'pointer',
-    },
-    }
